7e5abc7ddb24b95efaa0dbc5d93b84fc97db102d
woongbin-choi <woongbin96@naver.com>
2021-09-01 10:24:13 +0900

    Commit in/output 3ea

---
+++ BaekJoon/src/input_output/al_1001.java
@@ -0,0 +1,11 @@
+package input_output;
+import java.util.Scanner;
+
+public class al_1001 {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		System.out.println(A-B);
+	}
+}
---
+++ BaekJoon/src/input_output/al_1008.java
@@ -0,0 +1,12 @@
+package input_output;
+
+import java.util.Scanner;
+
+public class al_1008 {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println((double)A/B);
+	}
+}
