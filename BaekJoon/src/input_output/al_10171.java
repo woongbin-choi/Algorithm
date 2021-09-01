c9e5093adc69877b4e0a701f99fdae5a6c527b43
woongbin-choi <woongbin96@naver.com>
2021-09-01 10:25:01 +0900

    Commit in/output 4ea

---
+++ BaekJoon/src/input_output/al_10171.java
@@ -0,0 +1,10 @@
+package input_output;
+
+public class al_10171 {
+	public static void main(String[] args){
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
