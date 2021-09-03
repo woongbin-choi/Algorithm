97ef33fb9fa1e8b18a793b0d5d478ffb877d71b6
woongbin-choi <woongbin96@naver.com>
2021-09-03 13:05:31 +0900

    commit Algorithm num[2741]

---
+++ BaekJoon/src/For/al_2741.java
@@ -0,0 +1,22 @@
+package For;
+
+import java.io.*;
+
+public class al_2741 {
+	public static void main(String[] args) throws NumberFormatException,IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		for (int i = 1; i <= t; ++i) {
+			
+			bw.write(i+"\n");
+			
+		}
+		bw.flush();
+		
+		
+	}
+}
