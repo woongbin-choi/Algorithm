95b0bf7f1befccb4849b9c80a6002e1bea21df5d
woongbin-choi <woongbin96@naver.com>
2021-09-04 11:34:05 +0900

    commit Algorithm num[15552]

---
+++ BaekJoon/src/For/al_15552.java
@@ -0,0 +1,26 @@
+package For;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class al_15552 {
+	public static void main(String[] args) throws NumberFormatException,IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < t; ++i) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			bw.write(a+b+"\n");
+			
+		}
+		bw.flush();
+		
+		
+	}
+}
