9be252b3604a5cb80c01aebeee9e3707a051303a
woongbin-choi <woongbin96@naver.com>
2021-09-04 11:33:43 +0900

    commit Algorithm num[11021]

---
+++ BaekJoon/src/For/al_11021.java
@@ -0,0 +1,27 @@
+package For;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class al_11021 {
+	public static void main(String[] args) throws NumberFormatException,IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+	
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		for (int i = 1; i <= t; ++i) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			sb.append("Case #"+i+": "+ (a+b)+"\n");
+			
+		}
+		System.out.println(sb);
+		
+		
+	}
+}
