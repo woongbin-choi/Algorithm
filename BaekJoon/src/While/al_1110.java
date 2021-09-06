4d7ba8c0a6e34f35000fa4b619a9de683c9d4f10
woongbin-choi <woongbin96@naver.com>
2021-09-06 10:28:25 +0900

    commit Algorithm num[1110]

---
+++ BaekJoon/src/While/al_1110.java
@@ -0,0 +1,25 @@
+package While;
+
+import java.io.*;
+
+public class al_1110 {
+	public static void main(String[] args) throws NumberFormatException,IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int a = Integer.parseInt(br.readLine());
+		
+		int count = 0;
+		int res = a;
+		
+		while(true) {
+			int guess = ((a%10)*10 + (a/10+a%10)%10);
+			a = guess;
+			
+			count++;
+			if(guess == res) break;
+		}
+		
+		
+		System.out.println(count);
+	}
+}
