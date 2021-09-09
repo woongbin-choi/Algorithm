1383cdaca5d476dd3f9bc4fdf952fdadb6970115
woongbin-choi <woongbin96@naver.com>
2021-09-10 01:51:35 +0900

    commit Algorithm num[2675]

---
+++ BaekJoon/src/String/al_2675.java
@@ -0,0 +1,29 @@
+package String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class al_2675 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < T; ++i) {
+
+			String[] str = br.readLine().split(" ");
+
+			int R = Integer.parseInt(str[0]);
+			String S = str[1];
+
+			for (int j = 0; j < S.length(); ++j) {
+				for (int k = 0; k < R; ++k) {
+					System.out.print(S.charAt(j));
+				}
+			}
+
+			System.out.println();
+		}
+	}
+}
