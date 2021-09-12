2ee77822478d5eef6a182902f600d4b5017cd2ed
woongbin-choi <woongbin96@naver.com>
2021-09-12 14:13:57 +0900

    commit Algorithm num[5622]

---
+++ BaekJoon/src/String/al_5622.java
@@ -0,0 +1,55 @@
+package String;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class al_5622 {
+	// 다이얼 : 규칙에 따라 문자에 대응하는 수를 출력하는 문
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String s = br.readLine();
+		
+		int cnt = 0;
+		int k = s.length();
+		
+		for (int i = 0; i < k; ++i) {
+			
+			switch(s.charAt(i)) {
+			case 'A' : case 'B' : case 'C' :
+				cnt += 3;
+				break;
+				
+			case 'D' : case 'E' : case 'F' :
+				cnt += 4;
+				break;
+				
+			case 'G' : case 'H' : case 'I' :
+				cnt += 5;
+				break;
+				
+			case 'J' : case 'K' : case 'L' :
+				cnt += 6;
+				break;
+			
+			
+			case 'M' : case 'N' : case 'O' :
+				cnt += 7;
+				break;
+				
+			case 'P' : case 'Q' : case 'R' : case 'S' :
+				cnt += 8;
+				break;
+				
+			case 'T' : case 'U' : case 'V' :
+				cnt += 9;
+				break;
+				
+			case 'W' : case 'X' : case 'Y' : case 'Z' :
+				cnt += 10;
+				break;
+			}
+		}
+		System.out.println(cnt);
+	}
+}
