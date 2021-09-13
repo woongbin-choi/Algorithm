23bddcd008a6ae7cafe72a04c66ef3c622a74d4c
woongbin-choi <woongbin96@naver.com>
2021-09-13 10:33:40 +0900

    Commit Algorithm num[10809]

---
+++ BaekJoon/src/String/al_10809.java
@@ -0,0 +1,27 @@
+package String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class al_10809 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int[] arr = new int[26];
+		
+		for (int i = 0; i < arr.length; ++i) {
+			arr[i] = -1;
+		}
+		
+		String S = br.readLine();
+		
+		for (int i = 0; i < S.length(); ++i) {
+			char ch = S.charAt(i);
+			
+			if (arr[ch-'a'] == -1) arr[ch - 'a'] = i;
+		}
+		
+		for (int val : arr) System.out.print(val + " ");
+	}
+}
