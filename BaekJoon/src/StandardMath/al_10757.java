90cbfbb5320b6b4b3c819904ec4b7a83eb10ca2d
woongbin-choi <woongbin96@naver.com>
2021-11-15 11:07:58 +0900

    commit algorithm 10757

---
+++ BaekJoon/src/StandardMath/al_10757.java
@@ -0,0 +1,20 @@
+package StandardMath;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class al_10757 {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		BigInteger big1 = new BigInteger(st.nextToken());
+		BigInteger big2 = new BigInteger(st.nextToken());
+		
+		System.out.println(big1.add(big2));
+	}
+}
