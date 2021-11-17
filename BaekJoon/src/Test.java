794e6358a33fb9bb1ca5ba12531bf0d602f73dad
woongbin-choi <woongbin96@naver.com>
2021-11-17 14:15:33 +0900

    commit algorithm 2798

--- BaekJoon/src/Test.java
+++ BaekJoon/src/Test.java
@@ -1,51 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.StringTokenizer;
 
 public class Test {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(br.readLine());
 		
-		int[] arr = new int[N];
-		
-		st = new StringTokenizer(br.readLine(), " ");
+		int result = 0;
 		
 		for(int i = 0; i < N; ++i) {
-			arr[i] = Integer.parseInt(st.nextToken());
+			int num = i;
+			int sum = 0;
+			
+			while(num != 0) {
+				sum += num % 10;
+				num = num / 10;
+			}
+			
+			if (sum + i == N) {
+				result = i;
+				break;
+			}
 		}
-		
-		int result = search(arr, N, M);
 		
 		System.out.println(result);
 	}
 	
-	static int search(int[] arr, int N, int M) {
-		int result = 0;
-		
-		for(int i = 0; i < N - 2; ++i) {
-			for (int j = i+1; j < N - 1; ++j) {
-				for (int k = j+1; k < N; ++k) {
-					
-					int temp = arr[i] + arr[j] + arr[k];
-					
-					if (temp == M) return temp;
-					
-					if (result < temp && temp < M) result = temp;
-				}
-			}
-		}
-		
-		return result;
-	}
-
 }
