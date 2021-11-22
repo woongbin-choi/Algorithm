9cf91028e6b96c6e101c04ea1fe1a0af57629019
woongbin-choi <woongbin96@naver.com>
2021-11-22 10:51:15 +0900

    commit algorithm 7568

--- BaekJoon/src/BruteForce/al_7568.java
+++ BaekJoon/src/BruteForce/al_7568.java
@@ -1,10 +1,12 @@
+package BruteForce;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Test {
+public class al_7568 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		
@@ -39,5 +41,4 @@ public class Test {
 			System.out.print(rank + " ");
 		}
 	}
-	
 }
