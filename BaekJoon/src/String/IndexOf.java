282e8f197ea7a2295b82d69a773e2ed95f039784
woongbin <woongbin96@naver.com>
2022-10-12 09:40:11 +0900

    문장 속 단어

---
+++ BaekJoon/src/String/IndexOf.java
@@ -0,0 +1,39 @@
+package String;
+
+import java.util.Scanner;
+
+// 문장 속 단어
+// 한개 의 문장 속 가장 긴 단어를 출력 (가장 앞쪽에 위치한 단어)
+public class IndexOf {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String result = "";
+        int max = Integer.MIN_VALUE, pos;
+
+        while((pos=str.indexOf(' ')) != -1) {
+            String tmp = str.substring(0, pos);
+
+            int len = tmp.length();
+            if(len > max) {
+                max = len;
+                result = tmp;
+            }
+            str = str.substring(pos+1);
+        }
+        if(str.length() > max) {
+            result = str;
+        }
+
+//        String[] arr = str.split(" ");
+//        for (String s : arr){
+//            int len = s.length();
+//            if (len > max) {
+//                max = len;
+//                result = s;
+//            }
+//        }
+
+        System.out.println(result);
+    }
+}
