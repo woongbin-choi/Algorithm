ccb7671610f9d1be0a59083f25639a105f1b8d52
woongbin <woongbin96@naver.com>
2022-10-14 11:58:27 +0900

    회문 문자열

---
+++ BaekJoon/src/String/EpistemicString.java
@@ -0,0 +1,23 @@
+package String;
+
+import java.util.Scanner;
+
+// 회문 문자열 (대소문자 구분X)
+public class EpistemicString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        String result = "YES";
+        int len = str.length();
+
+        for(int i = 0; i < len/2; ++i){
+            if(str.charAt(i) != str.charAt(len -i - 1)){
+                result = "NO";
+                break;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
