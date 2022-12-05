f754b52259ca03717e3dfe4da6caa095d35de700
woongbin <woongbin96@naver.com>
2022-12-05 15:33:28 +0900

    중복 문자 제거

--- BaekJoon/src/Review/a6.java
+++ BaekJoon/src/Review/a6.java
@@ -1,21 +1,18 @@
-package String;
+package Review;
 
 import java.util.Scanner;
 
-// 중복문자 제거
-// indexOf -> 처음 발견한 문자의 인덱스 번호
-public class RemoveDupl {
+public class a6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
-        String result = "";
 
+        String result = "";
         for(int i = 0; i < str.length(); ++i) {
             if(str.indexOf(str.charAt(i)) == i) {
                 result += str.charAt(i);
             }
         }
-
         System.out.println(result);
     }
 }
