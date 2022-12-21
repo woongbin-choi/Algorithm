2e1c5d66f107540bb721ee34bd7ee0b2027654f6
woongbin <woongbin96@naver.com>
2022-12-22 08:35:22 +0900

    경로탐색(DFS)

--- BaekJoon/src/RecursiveTreeGraph/BFS.java
+++ BaekJoon/src/RecursiveTreeGraph/BFS.java
@@ -5,16 +5,16 @@ import java.util.Queue;
 
 // 넓이우선탐색
 public class BFS {
-    NodeBFS root;
-    public void BFS(NodeBFS root) {
-        Queue<NodeBFS> Q = new LinkedList<>();
+    Node root;
+    public void BFS(Node root) {
+        Queue<Node> Q = new LinkedList<>();
         Q.offer(root);
         int L = 0;
         while (!Q.isEmpty()) {
             int len = Q.size();
             System.out.print(L + " : ");
             for(int i = 0; i < len; ++i) {
-                NodeBFS current = Q.poll();
+                Node current = Q.poll();
                 System.out.print(current.data + " ");
                 if(current.lt != null) {
                     Q.offer(current.lt);
@@ -30,13 +30,13 @@ public class BFS {
 
     public static void main(String[] args) {
         BFS tree = new BFS();
-        tree.root=new NodeBFS(1);
-        tree.root.lt=new NodeBFS(2);
-        tree.root.rt=new NodeBFS(3);
-        tree.root.lt.lt=new NodeBFS(4);
-        tree.root.lt.rt=new NodeBFS(5);
-        tree.root.rt.lt=new NodeBFS(6);
-        tree.root.rt.rt=new NodeBFS(7);
+        tree.root=new Node(1);
+        tree.root.lt=new Node(2);
+        tree.root.rt=new Node(3);
+        tree.root.lt.lt=new Node(4);
+        tree.root.lt.rt=new Node(5);
+        tree.root.rt.lt=new Node(6);
+        tree.root.rt.rt=new Node(7);
         tree.BFS(tree.root);
     }
 }
