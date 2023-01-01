f903cdce72c2a6e19e3f09bc037bbbe40d649bf4
woongbin <woongbin96@naver.com>
2023-01-02 08:44:15 +0900

    동전교환

--- BaekJoon/src/DFS/ExchangeCoins.java
+++ BaekJoon/src/DFS/ExchangeCoins.java
@@ -1,12 +1,15 @@
 package DFS;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 // 동전 교환
 public class ExchangeCoins {
     static int n, m, result = Integer.MAX_VALUE;
-    public void DFS(int L, int sum, int[] arr) {
+    public void DFS(int L, int sum, Integer[] arr) {
         if(sum > m) return;
+        if(L >= result) return;
         if(sum == m) {
             result = Math.min(result, L);
         } else {
@@ -19,10 +22,11 @@ public class ExchangeCoins {
      ExchangeCoins T = new ExchangeCoins();
      Scanner sc = new Scanner(System.in);
      n = sc.nextInt();
-     int[] arr = new int[n];
+     Integer[] arr = new Integer[n];
      for(int i = 0; i < n; ++i) {
          arr[i] = sc.nextInt();
      }
+     Arrays.sort(arr, Collections.reverseOrder());
      m = sc.nextInt();
      T.DFS(0,0,arr);
      System.out.println(result);
