15b93c35d2a0bc0749094a039eafbb069b12795e
woongbin <woongbin96@naver.com>
2023-01-19 08:58:33 +0900

    단어 뒤집기

--- BaekJoon/src/String/ReverseWord.java
+++ BaekJoon/src/String/ReverseWord.java
@@ -6,47 +6,42 @@ import java.util.Scanner;
 
 // 단어 뒤집기, StringBuilder, 일일이 뒤집기
 public class ReverseWord {
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
-        String[] str = new String[n];
-        for(int i= 0; i<n; ++i){
-            str[i] = sc.next();
-        }
-
+    public ArrayList<String> solution(int n, String[] str) {
         ArrayList<String> result = new ArrayList<>();
         for(String x : str) {
             String tmp = new StringBuilder(x).reverse().toString();
             result.add(tmp);
         }
-        for(String y : result) {
-            System.out.println(y);
+//        일일이 뒤집기(특정 글자만 바꿀수 있다)
+//        ArrayList<String> result2 = new ArrayList<>();
+//        for(String z : str) {
+//            char[] s = z.toCharArray();
+//            int lt = 0;
+//            int rt = z.length() -1;
+//
+//            while(lt < rt) {
+//                char tmp = s[lt];
+//                s[lt] = s[rt];
+//                s[rt] = tmp;
+//                lt++;
+//                rt--;
+//            }
+//            String anwser = String.valueOf(s);
+//            result2.add(anwser);
+//        }
+        return result;
+    }
+    public static void main(String[] args) {
+        ReverseWord T = new ReverseWord();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] str = new String[n];
+        for(int i= 0; i<n; ++i){
+            str[i] = sc.next();
         }
-
-        // 일일이 뒤집기(특정 글자만 바꿀수 있다)
-        ArrayList<String> result2 = new ArrayList<>();
-        for(String z : str) {
-            char[] s = z.toCharArray();
-            int lt = 0;
-            int rt = z.length() -1;
-
-            while(lt < rt) {
-                char tmp = s[lt];
-                s[lt] = s[rt];
-                s[rt] = tmp;
-                lt++;
-                rt--;
-            }
-            String anwser = String.valueOf(s);
-            result2.add(anwser);
+        for(String x : T.solution(n, str)) {
+            System.out.println(x);
         }
-
-        for(String s : result2){
-            System.out.println(s);
-        }
-
     }
 
 }
