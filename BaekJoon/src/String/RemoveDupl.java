a05e699af0b01c5feea98519614fe0fbcf88d8a0
woongbin <woongbin96@naver.com>
2023-01-19 14:00:54 +0900

    중복문자 제거

--- BaekJoon/src/String/RemoveDupl.java
+++ BaekJoon/src/String/RemoveDupl.java
@@ -5,17 +5,19 @@ import java.util.Scanner;
 // 중복문자 제거
 // indexOf -> 처음 발견한 문자의 인덱스 번호
 public class RemoveDupl {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.next();
+    public String solution(String str) {
         String result = "";
-
         for(int i = 0; i < str.length(); ++i) {
             if(str.indexOf(str.charAt(i)) == i) {
                 result += str.charAt(i);
             }
         }
-
-        System.out.println(result);
+        return result;
+    }
+    public static void main(String[] args) {
+        RemoveDupl T = new RemoveDupl();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(T.solution(str));
     }
 }
