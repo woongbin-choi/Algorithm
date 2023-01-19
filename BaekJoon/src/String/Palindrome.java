af228ebcefa1ddba6a5f445f33f1cc39560848e5
woongbin <woongbin96@naver.com>
2023-01-19 14:14:21 +0900

    팰린드롬

--- BaekJoon/src/String/Palindrome.java
+++ BaekJoon/src/String/Palindrome.java
@@ -4,16 +4,18 @@ import java.util.Scanner;
 
 // 팰린드롬 - 회문
 public class Palindrome {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-
+    public String solution(String str) {
         String result = "NO";
         str = str.toUpperCase().replaceAll("[^A-Z]","");
-
         String tmp = new StringBuilder(str).reverse().toString();
         if (str.equals(tmp)) result ="YES";
 
-        System.out.println(result);
+        return result;
+    }
+    public static void main(String[] args) {
+        Palindrome T = new Palindrome();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(T.solution(str));
     }
 }
