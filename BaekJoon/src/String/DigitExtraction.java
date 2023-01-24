d2712023358e200bcb7151e7700bfedf69573971
woongbin <woongbin96@naver.com>
2023-01-25 08:04:57 +0900

    숫자만 추출

--- BaekJoon/src/String/DigitExtraction.java
+++ BaekJoon/src/String/DigitExtraction.java
@@ -4,28 +4,26 @@ import java.util.Scanner;
 
 // 숫자만 추출
 public class DigitExtraction {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-
-//        int result = 0;
-//
-//        for (char x : str.toCharArray()){
-//            if(x >= 48 && x <= 57){
-//                result = result * 10 + (x - 48);
-//            }
-//        }
-
+    public int solution(String str){
+        int result = 0;
         String s = "";
-
         for (char x : str.toCharArray()) {
             if (Character.isDigit(x)) {
                 s += x;
             }
         }
-
-        int result = Integer.parseInt(s);
-
-        System.out.println(result);
+//        for (char x : str.toCharArray()){
+//            if(x >= 48 && x <= 57){
+//                result = result * 10 + (x - 48);
+//            }
+//        }
+        result = Integer.parseInt(s);
+        return result;
+    }
+    public static void main(String[] args) {
+        DigitExtraction T = new DigitExtraction();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(T.solution(str));
     }
 }
