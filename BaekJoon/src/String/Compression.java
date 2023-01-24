564a53ba13a83d81687bf541e3cb4f80516e2bae
woongbin <woongbin96@naver.com>
2023-01-25 08:32:43 +0900

    문자열 압축

--- BaekJoon/src/String/Compression.java
+++ BaekJoon/src/String/Compression.java
@@ -4,10 +4,7 @@ import java.util.Scanner;
 
 // 문자열 압축
 public class Compression {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-
+    public String solution(String str) {
         String result = "";
         str += " ";
         int cnt = 1;
@@ -16,14 +13,19 @@ public class Compression {
             if(str.charAt(i) == str.charAt(i+1)){
                 cnt++;
             } else {
-              result += str.charAt(i);
-              if(cnt > 1) {
-                  result += String.valueOf(cnt);
-                  cnt = 1;
-              }
+                result += str.charAt(i);
+                if(cnt > 1) {
+                    result += String.valueOf(cnt);
+                    cnt = 1;
+                }
             }
         }
-
-        System.out.println(result);
+        return result;
+    }
+    public static void main(String[] args) {
+        Compression T = new Compression();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(T.solution(str));
     }
 }
