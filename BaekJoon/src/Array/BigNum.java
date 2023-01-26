f9de393e5b8adb26cc38c913207925b1b5960ed2
woongbin <woongbin96@naver.com>
2023-01-26 17:48:57 +0900

    큰 수 출력하기

--- BaekJoon/src/Array/BigNum.java
+++ BaekJoon/src/Array/BigNum.java
@@ -5,7 +5,18 @@ import java.util.Scanner;
 
 // 큰 수 출력하기
 public class BigNum {
+    public ArrayList<Integer> solution(int n, int[] arr) {
+        ArrayList<Integer> result = new ArrayList<>();
+        result.add(arr[0]);
+        for(int i = 0; i < n; ++i){
+            if(arr[i] > arr[i-1]) {
+                result.add(arr[i]);
+            }
+        }
+        return result;
+    }
     public static void main(String[] args) {
+        BigNum T = new BigNum();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
@@ -14,15 +25,7 @@ public class BigNum {
             arr[i] = sc.nextInt();
         }
 
-        ArrayList<Integer> result = new ArrayList<>();
-        result.add(arr[0]);
-        for(int i = 0; i < n; ++i){
-            if(arr[i] > arr[i-1]) {
-                result.add(arr[i]);
-            }
-        }
-
-        for(int x : result) {
+        for(int x : T.solution(n,arr)) {
             System.out.println(x);
         }
     }
