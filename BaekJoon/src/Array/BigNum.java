2aee8838831c00574fdb8f31f30fe839bd4239dd
woongbin <woongbin96@naver.com>
2023-01-26 17:49:41 +0900

    큰 수 출력하기

--- BaekJoon/src/Array/BigNum.java
+++ BaekJoon/src/Array/BigNum.java
@@ -8,7 +8,7 @@ public class BigNum {
     public ArrayList<Integer> solution(int n, int[] arr) {
         ArrayList<Integer> result = new ArrayList<>();
         result.add(arr[0]);
-        for(int i = 0; i < n; ++i){
+        for(int i = 1; i < n; ++i){
             if(arr[i] > arr[i-1]) {
                 result.add(arr[i]);
             }
