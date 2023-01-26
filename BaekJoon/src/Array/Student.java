a3b5548b7a780c0f635f472b66aabbd93618ca98
woongbin <woongbin96@naver.com>
2023-01-26 17:51:21 +0900

    보이는 학생

--- BaekJoon/src/Array/Student.java
+++ BaekJoon/src/Array/Student.java
@@ -4,16 +4,7 @@ import java.util.Scanner;
 
 // 보이는 학생
 public class Student {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-
-        for (int i = 0; i < n; ++i){
-            arr[i] = sc.nextInt();
-        }
-
+    public int solution(int n, int[] arr) {
         int max = 0;
         int result = 0;
 
@@ -23,7 +14,16 @@ public class Student {
                 max = arr[i];
             }
         }
-
-        System.out.println(result);
+        return result;
+    }
+    public static void main(String[] args) {
+        Student T = new Student();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; ++i){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(T.solution(n,arr));
     }
 }
