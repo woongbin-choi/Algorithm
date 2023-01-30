37a03f44027cbdad401a2e861384c560e28ab4de
woongbin <woongbin96@naver.com>
2023-01-30 17:34:17 +0900

    뒤집은 소수

--- BaekJoon/src/Array/ReverseMinority.java
+++ BaekJoon/src/Array/ReverseMinority.java
@@ -16,16 +16,7 @@ public class ReverseMinority {
         }
         return true;
     }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-
-        for(int i = 0; i < n; ++i){
-            arr[i] = sc.nextInt();
-        }
-
+    public ArrayList<Integer> solution(int n, int[] arr) {
         ArrayList<Integer> result = new ArrayList<>();
         for(int i = 0; i < n; ++i) {
             int tmp = arr[i];
@@ -39,9 +30,18 @@ public class ReverseMinority {
                 result.add(res);
             }
         }
-
-        System.out.println(result);
+        return result;
     }
-
-
+    public static void main(String[] args) {
+        ReverseMinority T = new ReverseMinority();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; ++i){
+            arr[i] = sc.nextInt();
+        }
+        for(int x : T.solution(n, arr)) {
+            System.out.print(x + " ");
+        }
+    }
 }
