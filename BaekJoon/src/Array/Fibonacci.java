993fe0250b5d1accadce3cf5ca492ed4d2436c89
woongbin <woongbin96@naver.com>
2023-01-30 12:39:42 +0900

    피보나치 수열

--- BaekJoon/src/Array/Fibonacci.java
+++ BaekJoon/src/Array/Fibonacci.java
@@ -5,10 +5,7 @@ import java.util.Scanner;
 // 피보나치 수열
 // 앞의 2개의 수를 합하여 다음 숫자가 되는 수열
 public class Fibonacci {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
+    public int[] solution(int n){
         int[] result = new int[n];
         result[0] = 1;
         result[1] = 1;
@@ -16,8 +13,13 @@ public class Fibonacci {
         for(int i = 2; i < n; ++i){
             result[i] = result[i-2] + result[i-1];
         }
-
-        for(int x : result) {
+        return result;
+    }
+    public static void main(String[] args) {
+        Fibonacci T = new Fibonacci();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int x : T.solution(n)) {
             System.out.println(x);
         }
     }
