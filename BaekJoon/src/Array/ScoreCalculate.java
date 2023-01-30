91e448871302c7899d7cace8d98733ee13967c60
woongbin <woongbin96@naver.com>
2023-01-30 17:37:53 +0900

    점수 계산하기

--- BaekJoon/src/Array/ScoreCalculate.java
+++ BaekJoon/src/Array/ScoreCalculate.java
@@ -4,17 +4,9 @@ import java.util.Scanner;
 
 // 점수 계산하기
 public class ScoreCalculate {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        for(int i = 0; i < n; ++i){
-            arr[i] = sc.nextInt();
-        }
-
+    public int solution(int n, int[] arr){
         int result = 0;
         int cnt = 0;
-
         for(int i = 0; i < n; ++i) {
             if(arr[i] == 1) {
                 cnt++;
@@ -23,7 +15,16 @@ public class ScoreCalculate {
                 cnt = 0;
             }
         }
-
-        System.out.println(result);
+        return result;
+    }
+    public static void main(String[] args) {
+        ScoreCalculate T = new ScoreCalculate();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; ++i){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(T.solution(n, arr));
     }
 }
