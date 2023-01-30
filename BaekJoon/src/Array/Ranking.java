c983545b1f5c37d22532d972b13b4d7e494511fd
woongbin <woongbin96@naver.com>
2023-01-30 17:40:04 +0900

    등수 구하기

--- BaekJoon/src/Array/Ranking.java
+++ BaekJoon/src/Array/Ranking.java
@@ -4,15 +4,7 @@ import java.util.Scanner;
 
 // 등수 구하기
 public class Ranking {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-
-        for(int i = 0; i < n; ++i) {
-            arr[i] = sc.nextInt();
-        }
-
+    public int[] solution(int n, int[] arr) {
         int[] result = new int[n];
         for(int i = 0; i< n; ++i) {
             int cnt = 1;
@@ -23,7 +15,18 @@ public class Ranking {
             }
             result[i] = cnt;
         }
-
-        System.out.println(result);
+        return result;
+    }
+    public static void main(String[] args) {
+        Ranking T = new Ranking();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; ++i) {
+            arr[i] = sc.nextInt();
+        }
+        for(int x : T.solution(n, arr)) {
+            System.out.print(x + " ");
+        }
     }
 }
