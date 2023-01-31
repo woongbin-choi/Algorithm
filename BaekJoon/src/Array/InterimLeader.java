951b44b1d795e3f9ad3f4abb7d2fdb6d42a44a3c
woongbin <woongbin96@naver.com>
2023-01-31 17:56:21 +0900

    임시반장 정하기

--- BaekJoon/src/Array/InterimLeader.java
+++ BaekJoon/src/Array/InterimLeader.java
@@ -4,16 +4,7 @@ import java.util.Scanner;
 
 // 임시 반장 정하기
 public class InterimLeader {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[][] arr = new int[n][n];
-        for(int i = 1; i < n; ++i){
-            for(int j = 1; j < n; ++j) {
-                arr[i][j] = sc.nextInt();
-            }
-        }
-
+    public int solution(int n, int[][] arr){
         int result = 0, max = Integer.MIN_VALUE;
         for(int i = 1; i <= n; ++i) {
             int cnt = 0;
@@ -25,13 +16,23 @@ public class InterimLeader {
                     }
                 }
             }
-
             if(cnt > max) {
                 max = cnt;
                 result = i;
             }
         }
-
-        System.out.println(result);
+        return result;
+    }
+    public static void main(String[] args) {
+        InterimLeader T = new InterimLeader();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][n];
+        for(int i = 1; i < n; ++i){
+            for(int j = 1; j < n; ++j) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println(T.solution(n, arr));
     }
 }
