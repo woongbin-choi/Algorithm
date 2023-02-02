193716385dd98921063c08e79774e8ecc0be9529
woongbin <woongbin96@naver.com>
2023-02-02 14:41:25 +0900

    버블 정렬

--- BaekJoon/src/Sorting/BubbleSort.java
+++ BaekJoon/src/Sorting/BubbleSort.java
@@ -4,14 +4,7 @@ import java.util.Scanner;
 
 // 버블정렬
 public class BubbleSort {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        for(int i = 0; i < n; ++i) {
-            arr[i] = sc.nextInt();
-        }
-
+    public int[] solution(int n, int[] arr){
         for(int i = 0; i < n-1; ++i) {
             for(int j = 0; j < n-i-1; ++j) {
                 if(arr[j] > arr[j+1]) {
@@ -21,8 +14,17 @@ public class BubbleSort {
                 }
             }
         }
-
-        for(int x : arr) {
+        return arr;
+    }
+    public static void main(String[] args) {
+        BubbleSort T = new BubbleSort();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; ++i) {
+            arr[i] = sc.nextInt();
+        }
+        for(int x : T.solution(n, arr)) {
             System.out.print(x + " ");
         }
     }
