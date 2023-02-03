9a4ab7f4ee78d5963f6277294a4ce77883ba46d6
woongbin <woongbin96@naver.com>
2023-02-03 10:07:16 +0900

    장난꾸러기

--- BaekJoon/src/Sorting/Imp.java
+++ BaekJoon/src/Sorting/Imp.java
@@ -6,14 +6,7 @@ import java.util.Scanner;
 
 // 장난꾸러기
 public class Imp {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        for(int i = 0; i < n; ++i) {
-            arr[i] = sc.nextInt();
-        }
-
+    public ArrayList<Integer> solution(int n, int[] arr){
         ArrayList<Integer> result = new ArrayList<>();
         int[] tmp = arr.clone();
         Arrays.sort(tmp);
@@ -23,8 +16,17 @@ public class Imp {
                 result.add(i+1);
             }
         }
-
-        for(int x : result) {
+        return result;
+    }
+    public static void main(String[] args) {
+        Imp T = new Imp();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; ++i) {
+            arr[i] = sc.nextInt();
+        }
+        for(int x : T.solution(n, arr)) {
             System.out.println(x + " ");
         }
     }
