fd59397052306e36b3a6671de0436eab64d96a81
woongbin <woongbin96@naver.com>
2023-02-13 09:04:18 +0900

    연속된 부분 수열

--- BaekJoon/src/Array/ContinuousSequence.java
+++ BaekJoon/src/Array/ContinuousSequence.java
@@ -4,15 +4,7 @@ import java.util.Scanner;
 
 //  연속 부분 수열(복합적 문제)
 public class ContinuousSequence {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int[] arr = new int[n];
-        for(int i = 0; i < n; ++i) {
-            arr[i] = sc.nextInt();
-        }
-
+    public int solution(int n, int m, int[] arr){
         int result = 0, sum = 0, lt = 0;
         for(int rt = 0; rt < n; ++rt) {
             sum += arr[rt];
@@ -27,6 +19,17 @@ public class ContinuousSequence {
                 }
             }
         }
-        System.out.println(result);
+        return result;
+    }
+    public static void main(String[] args) {
+        ContinuousSequence T = new ContinuousSequence();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; ++i) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(T.solution(n,m,arr));
     }
 }
