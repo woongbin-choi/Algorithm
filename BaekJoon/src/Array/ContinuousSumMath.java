d7ab1bb749a6d3f4f159d339886a8a99262873e6
woongbin <woongbin96@naver.com>
2023-02-14 10:52:12 +0900

    연속된 자연수의 합

--- BaekJoon/src/Array/ContinuousSumMath.java
+++ BaekJoon/src/Array/ContinuousSumMath.java
@@ -4,10 +4,7 @@ import java.util.Scanner;
 
 // 연속된 자연수의 합(수학적으로 풀이)
 public class ContinuousSumMath {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
+    public int solution(int n) {
         int result = 0, cnt = 1;
         n--;
         while(n > 0) {
@@ -17,7 +14,12 @@ public class ContinuousSumMath {
                 result++;
             }
         }
-
-        System.out.println(result);
+        return result;
+    }
+    public static void main(String[] args) {
+        ContinuousSumMath T = new ContinuousSumMath();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(T.solution(n));
     }
 }
